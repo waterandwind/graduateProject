1b9a3f2af3df1b7bd98773190d1d2171b2cefc1e
waterandwind <112683966@qq.com>
2019-12-22 13:29:14 +0800

    代码生成器测试完毕

---
+++ restaurant/src/main/java/com/baomidou/ant/commodity/service/ICommodityService.java
@@ -0,0 +1,16 @@
+package com.baomidou.ant.commodity.service;
+
+import com.baomidou.ant.commodity.entity.Commodity;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zyw
+ * @since 2019-12-22
+ */
+public interface ICommodityService extends IService<Commodity> {
+
+}
