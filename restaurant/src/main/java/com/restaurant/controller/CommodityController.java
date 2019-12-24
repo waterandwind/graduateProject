c1e37fc906efb42c615664d42d315018e930b239
waterandwind <112683966@qq.com>
2019-12-24 22:09:52 +0800

    添加文件上传显示

--- restaurant/src/main/java/com/restaurant/controller/CommodityController.java
+++ restaurant/src/main/java/com/restaurant/controller/CommodityController.java
@@ -2,13 +2,24 @@ package com.restaurant.controller;
 
 
 import com.restaurant.entity.Commodity;
+import com.restaurant.entity.result.Response;
 import com.restaurant.service.ICommodityService;
+import com.sun.deploy.net.URLEncoder;
 import io.swagger.annotations.ApiOperation;
+import org.apache.tomcat.jni.FileInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
 /**
  * <p>
@@ -30,5 +41,31 @@ public class CommodityController {
     public boolean saveCommodity(@RequestBody(required=true) Commodity commodity)  throws Exception{
         return  iCommodeytService.save(commodity);
     }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public Response upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return Response.bizError("上传失败，请选择文件");
+        }
+
+        String fileName = file.getOriginalFilename();
+        String filePath =System.getProperty("user.dir")+ "/src/main/resources/profile/";
+        File dest = new File(filePath + fileName);
+        try {
+            file.transferTo(dest);
+            return Response.success("上传成功");
+        } catch (IOException e) {
+          return Response.bizError("未知异常");
+        }
+    }
+    @GetMapping(value = "/imgShow", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> imgShow() throws FileNotFoundException {
+        InputStream inputStream=new FileInputStream(new File("E:\\毕业设计\\毕业设计\\graduateProject\\restaurant\\src\\main\\resources\\profile\\微信图片_20171112112426.jpg"));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders headers = new HttpHeaders();
+        return new ResponseEntity<>(inputStreamResource,headers, HttpStatus.OK);
+    }
+
 }
 
