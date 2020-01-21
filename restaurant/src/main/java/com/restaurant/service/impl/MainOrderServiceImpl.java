c129aa8892851de8724b37a2457ae889249b036d
waterandwind <112683966@qq.com>
2020-01-21 21:58:11 +0800

    删除多余import

--- restaurant/src/main/java/com/restaurant/service/impl/MainOrderServiceImpl.java
+++ restaurant/src/main/java/com/restaurant/service/impl/MainOrderServiceImpl.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sun.applet.Main;
+
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
